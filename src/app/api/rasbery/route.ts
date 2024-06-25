54b2aa7f532999a397d55cb148f1445046750b1b
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-06-25 22:25:12 +0900

    add

---
+++ src/app/api/rasbery/route.ts
@@ -0,0 +1,17 @@
+import { database } from "@/lib/firebase/firebase";
+import { set, ref } from "firebase/database";
+
+// ユーザーデータをデータベースに書き込む非同期関数
+async function writeUserData(userId: string) {
+  await set(ref(database, "users"), {
+    userId: userId,
+  });
+}
+
+// APIルートのハンドラー関数
+export async function POST(request: Request) {
+  const res = await request.json();
+  await writeUserData(res.userId);
+
+  return Response.json({ res });
+}
---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,30 @@
+import "firebase/compat/database";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyACTSRf4rSTsdy9CXO0uNFHVaTwwrFfTDM",
+  authDomain: "reserveapp-c0dbd.firebaseapp.com",
+  databaseURL: "https://reserveapp-c0dbd-default-rtdb.firebaseio.com",
+  projectId: "reserveapp-c0dbd",
+  storageBucket: "reserveapp-c0dbd.appspot.com",
+  messagingSenderId: "314286825120",
+  appId: "1:314286825120:web:4b5014ee0cc47fac23b3c5",
+  measurementId: "G-BRJXP0DDRJ",
+};
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+
+// Initialize Realtime Database and get a reference to the service
+
+// Initialize Firebase
