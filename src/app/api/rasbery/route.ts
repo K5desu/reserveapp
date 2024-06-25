c11df59760b1f84812041481676a20bfd17036fe
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-06-25 20:03:14 +0900

    errorhandle

--- src/app/api/rasbery/route.ts
+++ src/app/api/rasbery/route.ts
@@ -1,29 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/lib/prismaclient";
 import { database } from "@/lib/firebase/firebase";
 import { ref, set } from "firebase/database";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  function writeUserData(userId: string) {
-    set(ref(database, "users"), {
-      userId: userId,
-    });
-  }
-  if (req.method === "POST") {
-    // Handle POST request
-    const createdData = writeUserData(req.body.userId);
-    res.status(200).json(createdData);
-  } else {
-    res.status(400).json({ message: "Method Not Allowed" });
-  }
-  if (req.method === "GET") {
-    // Handle GET request
-    const data = await prisma.user.findMany();
-    res.status(200).json(data);
-  } else {
-    res.status(405).json({ message: "Method Not Allowed" });
-  }
+// ユーザーデータをデータベースに書き込む非同期関数
+async function writeUserData(userId: string) {
+  await set(ref(database, "users"), {
+    userId: userId,
+  });
+}
+
+// APIルートのハンドラー関数
+export async function POST(request: Request) {
+  const res = await request.json();
+  await writeUserData(res.userId);
+
+  return Response.json({ res });
 }
