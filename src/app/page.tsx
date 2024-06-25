2d06440cfc3b16a570d104eaa527df2f6be95516
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-06-25 20:46:59 +0900

    errorhandle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-import { ref, onValue, set } from "firebase/database";
-import { database } from "@/lib/firebase/firebase";
-import { useState } from "react";
 export default function Home() {
-  const [data, setData] = useState();
-  const starCountRef = ref(database, "user");
-  onValue(starCountRef, (snapshot) => {
-    const data = snapshot.val();
-    setData(data);
-  });
-  return <main>{data}</main>;
+  return <main></main>;
 }
