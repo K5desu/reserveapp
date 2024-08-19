5e6d924fcdbdc0e1a2fdc85313484cabff21fc76
K5desu <dekinousyonn@gmail.com>
2024-08-19 18:47:44 +0900

    a

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,31 +4,18 @@ import { useState } from "react";
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date());
   return (
-    <main className="flex justify-between p-4 bg-white border-2 border-gray-300">
-      <div className="w-1/2">
-        <div className="text-center text-red-500">
-          <h2>使用状況</h2>
-        </div>
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="rounded-md border"
-        />
+    <main className="flex flex-col items-center p-4 bg-white border-2 border-gray-300">
+      <div className="w-full h-full relative">
+        <div className="w-12 h-11 absolute left-0 top-0 bg-red-600"></div>
       </div>
-      <div className="w-1/2">
-        <div className="text-center">
-          <h3>おしらせ</h3>
-        </div>
-        <div className="border border-gray-300 p-2 bg-gray-100 mb-2 flex justify-between items-center">
-          <p>MM/DD 本日は開館日です。</p>
-          <button className="bg-black text-white px-2 py-1">削除</button>
-        </div>
-        <div className="border border-gray-300 p-2 bg-gray-100 flex justify-between items-center">
-          <p>MM/DD 本日は米国です。</p>
-          <button className="bg-black text-white px-2 py-1">削除</button>
-        </div>
-      </div>
+      <h2 className="text-red-500">使用状況</h2>
+
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
     </main>
   );
 }
