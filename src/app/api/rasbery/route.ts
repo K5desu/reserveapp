1dff0e8a5c63634ae53c6de3085eb6cf8bf4a9f1
K5desu <dekinousyonn@gmail.com>
2024-10-05 23:46:31 +0900

    errorhandle

--- src/app/api/rasbery/route.ts
+++ src/app/api/rasbery/route.ts
@@ -15,9 +15,9 @@ async function writeUserData(res: string) {
 // APIルートのハンドラー関数
 export async function POST(request: Request) {
   const req = await request.json();
-  if (req.uuid == process.env.RASBERRY_UUID) {
+  if (req.uuid == "c756d38d-2c5d-4e2e-9a69-45d1184cc08a") {
     try {
-      await writeUserData(req.userId);
+      await writeUserData(req.student_id);
       return new Response("OK");
     } catch (e) {
       return new Response("学籍番号の書き込みに失敗しました");
